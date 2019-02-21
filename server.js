d5de922d3c21d4099bd7339c51e2a2787eb7faec
Riley-Brown <riley@riley.gg>
2019-02-20 23:34:11 -0500

    created express server for retrieving photos from unsplash api

---
+++ server.js
@@ -0,0 +1,23 @@
+global.fetch = require('node-fetch');
+const config = require('universal-config');
+const Unsplash = require('unsplash').default();
+const toJson = require('unsplash').toJson();
+const express = require('express');
+
+const Unsplash = new Unsplash({
+  applicationID: config.get('APPLICATION_ID'),
+  secret: config.get('SECRET'),
+  callback_url: config.get('CALLBACK_URL')
+})
+
+const app = express();
+
+app.get('/api/photos', (req, res) => {
+  unsplash.photos.listPhotos(1, 30)
+    .then(toJson)
+    .then(json => res.json(json))
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
