1581b94b559ac1dabadcf9f0846e7088be6943e6
Riley-Brown <riley@riley.gg>
2019-02-22 01:10:46 -0500

    maping over images state and passing props to image component

--- client/src/components/Images.js
+++ client/src/components/Images.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import InfiniteScroll from 'react-infinite-scroll-component';
+import Image from './Image';
 
 export default class Images extends Component {
   state = {
@@ -10,17 +11,29 @@ export default class Images extends Component {
   };
   componentDidMount() {
     const { count, start } = this.state;
+
+    // initial api call for images
     axios.get(`/api/photos?count=${count}&start=${start}`).then(res =>
       this.setState({
         images: res.data
       })
     );
   }
+
   render() {
     console.log(this.state);
     return (
-      <div>
-        <h1>Hello</h1>
+      <div className="images">
+        <InfiniteScroll
+          dataLength={this.state.images.length}
+          next={this.fetchImages}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+        >
+          {this.state.images.map(image => (
+            <Image key={image.id} image={image} />
+          ))}
+        </InfiniteScroll>
       </div>
     );
   }
